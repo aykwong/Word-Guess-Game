75f335708232cc2377a8c2968e36b51cccb8f5d6
Kwong <aykwong93@gmail.com>
2018-06-04 15:04:17 -0400

    Completed JS to reset after completing guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,19 @@
 var stats = {
     wins: 0,
     losses: 0,
-    guessesLeft: 9,
+    guessesLeft: 10,
     guessDisplay: "_ ",
     guessesMade: "",
+    fillInCounter: 0,
     result: 0,
-    pokemon: ["pikachu", "charmander", "squirtle", "bulbasaur", "mewtwo", "charizard", "scyther", "gengar", "alakazam"],
-    guessWord: function() {
+    pokemon: ["pikachu", "charmander", "squirtle", "bulbasaur", "mewtwo", "charizard", "golem", "gengar", "alakazam", "dragonite", "geodude", "gyrados", "ditto"],
+    guessWord: function () {
         return this.pokemon[Math.floor(Math.random() * this.pokemon.length)]
     },
 };
 
+// Initial start-up before user interacts
 var computerGuess = stats.guessWord();
-
 document.getElementById("wins").innerHTML = stats.wins;
 document.getElementById("losses").innerHTML = stats.losses;
 document.getElementById("guessesLeft").innerHTML = stats.guessesLeft;
@@ -24,6 +25,7 @@ console.log(stats.guessDisplay);
 
 document.onkeyup = function userGuess(event) {
 
+    // Check to see if user has made that guess before
     var userGuess = event.key;
     for (var i = 0; i < stats.guessesMade.length; i++) {
         if (userGuess === stats.guessesMade[i]) {
@@ -32,6 +34,7 @@ document.onkeyup = function userGuess(event) {
         }
     }
 
+    // Check if user input matches any of the characters of the word
     for (var check = 0; check < computerGuess.length; check++) {
         if (userGuess === computerGuess[check]) {
             var sliceEnd = check * 2
@@ -39,16 +42,16 @@ document.onkeyup = function userGuess(event) {
             stats.guessDisplay = stats.guessDisplay.slice(0, sliceEnd) + userGuess + stats.guessDisplay.slice(sliceCheck);
             console.log(stats.guessDisplay);
             document.getElementById("guessDisplay").innerHTML = stats.guessDisplay;
+            stats.fillInCounter++;
         }
     }
 
-    if (userGuess === stats.guessWord) {
-        stats.guessesLeft--;
-        document.getElementById("guessesLeft").innerHTML = stats.guessesLeft;
+    // If user has completed guessing the word, wins go up by 1, change result counter to 1, if not, reduce guesses by 1
+    if (stats.fillInCounter === computerGuess.length) {
         stats.wins++;
         document.getElementById("wins").innerHTML = stats.wins;
         stats.result = 1;
-        alert("Congratulations! You guessed the letter, " + stats.guessWord + "." + " It took you " + (9 - stats.guessesLeft) + " tries.");
+        alert("Congratulations! You guessed the pokemon, " + computerGuess + ".");
     } else {
         stats.guessesLeft--;
         document.getElementById("guessesLeft").innerHTML = stats.guessesLeft;
@@ -56,18 +59,26 @@ document.onkeyup = function userGuess(event) {
         document.getElementById("guessesMade").innerHTML = stats.guessesMade;
     }
 
+    // If user has ran out of guesses, losses go up by 1, change result counter to 1
     if (stats.guessesLeft === 0) {
         stats.losses++;
         document.getElementById("losses").innerHTML = stats.losses;
         stats.result = 1;
     }
 
+    //if result counter equals 1, reset the game, change result counter to 0
     if (stats.result === 1) {
+        debugger;
         computerGuess = stats.guessWord();
-        stats.guessesLeft = 9;
+        console.log(computerGuess);
+        stats.guessesLeft = 10;
         document.getElementById("guessesLeft").innerHTML = stats.guessesLeft;
-        stats.guessesMade = [];
+        stats.guessesMade = "";
         document.getElementById("guessesMade").innerHTML = stats.guessesMade;
+        stats.guessDisplay = "_ ".repeat(computerGuess.length);
+        console.log(stats.guessDisplay);
+        document.getElementById("guessDisplay").innerHTML = stats.guessDisplay;
         stats.result = 0;
+        stats.fillInCounter = 0;
     }
 }
